a7a78abbb09cd1483ec28b5ff2b35a9a1c674c7e
felix1251 <domhack123@gmail.com>
2023-06-21 22:41:21 +0800

    use yew classes

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -3,6 +3,8 @@ use yew::prelude::*;
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
-        <h1 class="text-secondary">{"This is Home page"}</h1>
+        <h1 class={classes!("text-secondary")}>
+            {"This is Home page"}
+        </h1>
     }
 }
