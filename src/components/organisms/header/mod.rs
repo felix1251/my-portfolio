396b62c1897f18d050464e83f87df43faa254518
fabacajen <fabacajen@gotextbooks.com>
2023-07-11 16:05:56 +0800

    change container size

--- src/components/organisms/header/mod.rs
+++ src/components/organisms/header/mod.rs
@@ -6,10 +6,8 @@ use yew_router::prelude::*;
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
-        <header
-            class="w-full border-b transition-colors dark:border-gray-700 duration-200"
-        >
-            <div class="mx-auto max-w-[87rem] px-6 md:px-8 flex h-[4.8rem] items-center justify-between">
+        <header class="w-full border-b transition-colors dark:border-gray-700 duration-200">
+            <div class="mx-auto max-w-[85rem] px-6 md:px-8 flex h-[4.8rem] items-center justify-between">
                 <Link<Route> classes="font-medium text-secondary dark:text-white transition-colors duration-200 text-[18px] cursor-pointer" to={Route::Home}>
                     {"felixabacajen"}
                     <span class="text-primary">{".com"}</span>
