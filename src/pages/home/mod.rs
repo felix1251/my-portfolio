ce0f7440592bba477860fbc7ba7b96e47108cfee
felix1251 <domhack123@gmail.com>
2023-07-12 02:12:35 +0800

    ..

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -5,7 +5,7 @@ pub fn home() -> Html {
     html! {
         <>
             <h1 class={classes!("text-secondary")}>
-                {"This is Home page"}
+                {"This is home page"}
             </h1>
             <p class="text-orange-500">{"Me felix"}</p>
         </>
