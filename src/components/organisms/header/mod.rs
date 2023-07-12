8eb7cc2b95686cad9e33eef82ebf47269b053cef
felix1251 <domhack123@gmail.com>
2023-07-13 00:50:37 +0800

    logo text component

--- src/components/organisms/header/mod.rs
+++ src/components/organisms/header/mod.rs
@@ -1,18 +1,15 @@
 use crate::components::atoms::header_link::HeaderLink;
+use crate::components::atoms::logo_text::LogoText;
 use crate::components::atoms::theme_switcher::ThemeSwitcher;
 use crate::router::Route;
 use yew::prelude::*;
-use yew_router::prelude::*;
 
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
         <header class="w-full border-b transition-colors dark:border-gray-700 duration-200">
             <div class="mx-auto max-w-[85rem] px-6 md:px-8 flex h-[4.8rem] items-center justify-between">
-                <Link<Route> classes="font-medium text-secondary dark:text-white transition-colors duration-200 text-[18px] cursor-pointer" to={Route::Home}>
-                    {"felixabacajen"}
-                    <span class="text-primary">{".com"}</span>
-                </Link<Route>>
+                <LogoText />
                 <div class="flex gap-6 text-[16px] primary-text-color font-medium items-center">
                     <HeaderLink link={Route::Home}>{"Home"}</HeaderLink>
                     <HeaderLink link={Route::Projects}>{"Projects"}</HeaderLink>
