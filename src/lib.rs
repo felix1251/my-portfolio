5a2f1da32380c9b074911ec12f48314a4ef968cb
felix1251 <domhack123@gmail.com>
2023-07-14 02:29:49 +0800

    remove ds store

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ mod state;
 use components::templates::primary_layout::PrimaryLayout;
 use router::{switch, Route};
 use yew::prelude::*;
-use yew_router::history::{AnyHistory, History, MemoryHistory};
 use yew_router::prelude::*;
 
 #[function_component(App)]
@@ -19,21 +18,4 @@ pub fn app() -> Html {
             </PrimaryLayout>
         </BrowserRouter>
     }
-}
-
-#[derive(Properties, PartialEq, Debug)]
-pub struct ServerAppProps {
-    pub url: AttrValue,
-}
-
-#[function_component(ServerApp)]
-pub fn server_app(props: &ServerAppProps) -> Html {
-    let history = AnyHistory::from(MemoryHistory::new());
-    history.push(&*props.url);
-
-    return html! {
-        <Router history={history}>
-            <Switch<Route> render={switch} />
-        </Router>
-    };
 }
