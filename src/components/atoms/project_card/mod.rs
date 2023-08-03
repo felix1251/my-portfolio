e9965d4815996bd5af0051c30f46925219f621fa
felix1251 <domhack123@gmail.com>
2023-08-03 14:32:23 +0800

    change dark mode colors

--- src/components/atoms/project_card/mod.rs
+++ src/components/atoms/project_card/mod.rs
@@ -10,7 +10,7 @@ pub struct Props {
 pub fn project_card(props: &Props) -> Html {
     html! {
         <div class="p-6 rounded-2xl bg-quaternary dark:bg-dark-2">
-            <div class="h-[200px] w-full duration-200 bg-white dark:bg-dark relative overflow-hidden mb-3 rounded-xl">
+            <div class="relative h-[200px] duration-200 bg-white dark:bg-dark relative overflow-hidden mb-3 rounded-xl">
             </div>
             <div class="font-semibold text-lg mb-2 transition-colors duration-200 text-black dark:text-quaternary">
                 { props.title.clone() }
--- src/components/organisms/footer/mod.rs
+++ src/components/organisms/footer/mod.rs
@@ -4,10 +4,10 @@ use yew_icons::{Icon, IconId};
 #[function_component(Footer)]
 pub fn footer() -> Html {
     html! {
-        <footer class="w-full py-10 mx-auto max-w-[85rem] px-6 md:px-8 md:flex md:items-center md:justify-between">
-            <span class="text-[15px] text-tertiary sm:text-center font-medium">
+        <footer class="space-y-3 w-full py-6 md:py-10 mx-auto max-w-[85rem] px-6 md:px-8 md:flex md:items-center md:justify-between">
+            <div class="text-[15px] text-tertiary sm:text-center font-medium">
                 {"© 2023 Felix's Pages"}
-            </span>
+            </div>
             <div class="flex space-x-4 sm:justify-center md:mt-0">
                 <Icon class="text-tertiary hover:text-primary" icon_id={IconId::BootstrapGithub}/>
             </div>
