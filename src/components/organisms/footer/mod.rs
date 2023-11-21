7a43c101ab2e8fe98def44c11a767c98ff7980c2
felix1251 <domhack123@gmail.com>
2023-11-22 01:35:53 +0800

    add github icon in profile card

--- src/components/organisms/footer/mod.rs
+++ src/components/organisms/footer/mod.rs
@@ -1,5 +1,4 @@
 use yew::prelude::*;
-use yew_icons::{Icon, IconId};
 
 #[function_component(Footer)]
 pub fn footer() -> Html {
@@ -7,9 +6,6 @@ pub fn footer() -> Html {
     <footer class="space-y-3 w-full mx-auto max-w-4xl px-6 md:px-8 items-center flex justify-between">
       <div class="text-[15px] text-tertiary sm:text-center font-medium">
           {"© 2023 Felix's Pages"}
-      </div>
-      <div class="flex space-x-4 sm:justify-center md:mt-0">
-          <Icon class="text-tertiary hover:text-primary" icon_id={IconId::BootstrapGithub}/>
       </div>
     </footer>
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,6 @@ module.exports = {
         primary: "#0BB981",
         secondary: "#31274b",
         tertiary: "#AAAAAA",
-        quaternary: "#f6f7f8",
         dark: "#121212",
         "dark-2": "#1F1F1F",
       },
