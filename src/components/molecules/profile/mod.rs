fae10c72bad57c7496991979812df81f28670c20
felix1251 <domhack123@gmail.com>
2023-11-23 23:46:31 +0800

    update profile image size

--- src/components/molecules/profile/mod.rs
+++ src/components/molecules/profile/mod.rs
@@ -7,7 +7,7 @@ pub fn profile() -> Html {
    <div class="rounded-xl bg-dark-2 w-full drop-shadow-sm border border-dark-2">
       <div class="h-52 w-full bg-cover bg-center rounded-t-xl" style="background-image: url('assets/img/cover.jpeg');"></div>
       <div class="p-6">
-        <img class="ring-[5px] ring-dark-2 bg-tertiary -mt-28 w-44 h-44 rounded-full" src="assets/img/profile.jpeg" alt="profile"/>
+        <img class="ring-[5.5px] ring-dark-2 bg-tertiary -mt-32 w-40 h-40 rounded-full" src="assets/img/profile.jpeg" alt="profile"/>
         <div class="flex gap-1.5 items-center mt-2">
           <h1 class="text-[1.68rem]">{"Felix Abacajen"}</h1>
           <div class="text-[15px] text-tertiary">{"(He/Him)"}</div>
